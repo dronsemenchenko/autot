60cf5bce4618fecd15ae38289481f375c93939ac
Andrey Semenchenko <a.semenchenko@gmail.com>
2024-01-09 22:50:53 +0400

    php

--- docker/dev/fill-db.sh
+++ docker/dev/fill-db.sh
@@ -3,5 +3,5 @@ cd /var/www/;
 composer install;
 php bin/console doctrine:schema:create;
 php bin/console doctrine:fixtures:load -n;
-bin/console --env=test doctrine:schema:create;
+php bin/console --env=test doctrine:schema:create;
 php bin/console --env=test doctrine:fixtures:load -n;
