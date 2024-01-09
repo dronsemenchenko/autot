496ca12af5b6f437e3f5ef74646354ffe6232e71
Andrey Semenchenko <a.semenchenko@gmail.com>
2024-01-09 14:48:53 +0400

    fix fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,14 +15,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $citys=['Москва', 'Санкт-Петербург', 'Ростов-на-Дону', 'Казань'];
+        $citys = ['Москва', 'Санкт-Петербург', 'Ростов-на-Дону', 'Казань'];
         for ($i = 0; $i < sizeof($citys); $i++) {
             $city = new City();
             $city->setName($citys[$i]);
             $manager->persist($city);
         }
 
-        $purshaseStatuss=['Ожидает обработки', 'Ожидает отправки', 'Отправлено', 'Выдано клиенту'];
+        $purshaseStatuss = ['Ожидает обработки', 'Ожидает отправки', 'Отправлено', 'Выдано клиенту'];
         for ($i = 0; $i < sizeof($purshaseStatuss); $i++) {
             $purshaseStatus = new PurshaseStatus();
             $purshaseStatus->setName($purshaseStatuss[$i]);
@@ -35,27 +35,27 @@ class AppFixtures extends Fixture
             $product->setName($products[$i]);
             $manager->persist($product);
         }
-        
+
         $manager->flush();
 
         $storages = [
             [
-                'name'=>'Балашиха склад №1',
-                'city'=>'Москва',
-                'street'=>'Ленина',
-                'houseNum'=>12
+                'name' => 'Балашиха склад №1',
+                'city' => 'Москва',
+                'street' => 'Ленина',
+                'houseNum' => 12
             ],
             [
-                'name'=>'Ростов Cклад №2',
-                'city'=>'Ростов-на-Дону',
-                'street'=>'Вакуленко',
-                'houseNum'=>12
+                'name' => 'Ростов Cклад №2',
+                'city' => 'Ростов-на-Дону',
+                'street' => 'Вакуленко',
+                'houseNum' => 12
             ],
             [
-                'name'=>'Казань склад №3',
-                'city'=>'Казань',
-                'street'=>'Аигель',
-                'houseNum'=>33
+                'name' => 'Казань склад №3',
+                'city' => 'Казань',
+                'street' => 'Аигель',
+                'houseNum' => 33
             ],
         ];
         for ($i = 0; $i < sizeof($storages); $i++) {
@@ -72,39 +72,39 @@ class AppFixtures extends Fixture
 
         $productInStorages = [
             [
-                'storage'=>'Балашиха склад №1',
-                'product'=>'Аккумулятор Solite 44B19LBH',
-                'amount'=>100
+                'storage' => 'Балашиха склад №1',
+                'product' => 'Аккумулятор Solite 44B19LBH',
+                'amount' => 100
             ],
             [
-                'storage'=>'Балашиха склад №1',
-                'product'=>'Аккумулятор Sznajder 54570',
-                'amount'=>100
+                'storage' => 'Балашиха склад №1',
+                'product' => 'Аккумулятор Sznajder 54570',
+                'amount' => 100
             ],
             [
-                'storage'=>'Балашиха склад №1',
-                'product'=>'Инструмент, разное Bahco 9048250',
-                'amount'=>100
+                'storage' => 'Балашиха склад №1',
+                'product' => 'Инструмент, разное Bahco 9048250',
+                'amount' => 100
             ],
             [
-                'storage'=>'Балашиха склад №1',
-                'product'=>'Ручной инструмент Stanley 083179',
-                'amount'=>100
+                'storage' => 'Балашиха склад №1',
+                'product' => 'Ручной инструмент Stanley 083179',
+                'amount' => 100
             ],
             [
-                'storage'=>'Ростов Cклад №2',
-                'product'=>'Аккумулятор Solite 44B19LBH',
-                'amount'=>200
+                'storage' => 'Ростов Cклад №2',
+                'product' => 'Аккумулятор Solite 44B19LBH',
+                'amount' => 200
             ],
             [
-                'storage'=>'Ростов Cклад №2',
-                'product'=>'Ручной инструмент Stanley 083179',
-                'amount'=>200
+                'storage' => 'Ростов Cклад №2',
+                'product' => 'Ручной инструмент Stanley 083179',
+                'amount' => 200
             ],
             [
-                'storage'=>'Казань склад №3',
-                'product'=>'Аккумулятор Solite 44B19LBH',
-                'amount'=>300
+                'storage' => 'Казань склад №3',
+                'product' => 'Аккумулятор Solite 44B19LBH',
+                'amount' => 300
             ]
         ];
 
@@ -115,7 +115,7 @@ class AppFixtures extends Fixture
             $product = $manager->getRepository(Product::class)->findOneByName($productInStorages[$i]['product']);
             $productInStorage->setProduct($product);
             $productInStorage->setAmount($productInStorages[$i]['amount']);
-            
+
             $manager->persist($productInStorage);
         }
 
